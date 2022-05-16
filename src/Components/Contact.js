ae1017aa205078fd1837563a820ac28c8ee6f4df
Calum Bradley <cal@gmx.co.uk>
2022-05-16 17:05:45 +0100

    changes for build

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -6,12 +6,12 @@ class Contact extends Component {
     if (!this.props.data) return null
 
     const name = this.props.data.name
-    const street = this.props.data.address.street
+    // const street = this.props.data.address.street
     const city = this.props.data.address.city
     const state = this.props.data.address.state
-    const zip = this.props.data.address.zip
+    // const zip = this.props.data.address.zip
     const phone = this.props.data.phone
-    const message = this.props.data.contactmessage
+    // const message = this.props.data.contactmessage
 
     return (
       <section id="contact">
@@ -24,7 +24,7 @@ class Contact extends Component {
             </div>
 
             <div className="ten columns">
-              <p className="lead">{message}</p>
+              <p className="lead"></p>
             </div>
           </div>
         </Fade>
@@ -32,7 +32,7 @@ class Contact extends Component {
         <div className="row">
           <Slide left duration={1000}>
             <div className="eight columns">
-              <form action="" method="post" id="contactForm" name="contactForm">
+              <form id="contactForm" name="contactForm">
                 <fieldset>
                   <div>
                     <label htmlFor="contactName">
@@ -44,7 +44,6 @@ class Contact extends Component {
                       size="35"
                       id="contactName"
                       name="contactName"
-                      onChange={this.handleChange}
                     />
                   </div>
 
@@ -58,7 +57,6 @@ class Contact extends Component {
                       size="35"
                       id="contactEmail"
                       name="contactEmail"
-                      onChange={this.handleChange}
                     />
                   </div>
 
@@ -70,7 +68,6 @@ class Contact extends Component {
                       size="35"
                       id="contactSubject"
                       name="contactSubject"
-                      onChange={this.handleChange}
                     />
                   </div>
 
@@ -110,40 +107,40 @@ class Contact extends Component {
                 <p className="address">
                   {name}
                   <br />
-                  {city}, {state} {zip}
+                  {city}, {state}
                   <br />
                   <span>{phone}</span>
                 </p>
               </div>
 
               {/* <div className="widget widget_tweets">
-                <h4 className="widget-title">Latest Tweets</h4>
-                <ul id="twitter">
-                  <li>
-                    <span>
-                      This is Photoshop's version of Lorem Ipsum. Proin gravida
-                      nibh vel velit auctor aliquet. Aenean sollicitudin, lorem
-                      quis bibendum auctor, nisi elit consequat ipsum
-                      <a href="./">http://t.co/CGIrdxIlI3</a>
-                    </span>
-                    <b>
-                      <a href="./">2 Days Ago</a>
-                    </b>
-                  </li>
-                  <li>
-                    <span>
-                      Sed ut perspiciatis unde omnis iste natus error sit
-                      voluptatem accusantium doloremque laudantium, totam rem
-                      aperiam, eaque ipsa quae ab illo inventore veritatis et
-                      quasi
-                      <a href="./">http://t.co/CGIrdxIlI3</a>
-                    </span>
-                    <b>
-                      <a href="./">3 Days Ago</a>
-                    </b>
-                  </li>
-                </ul>
-              </div> */}
+                 <h4 className="widget-title">Latest Tweets</h4>
+                 <ul id="twitter">
+                   <li>
+                     <span>
+                       This is Photoshop's version of Lorem Ipsum. Proin gravida
+                       nibh vel velit auctor aliquet. Aenean sollicitudin, lorem
+                       quis bibendum auctor, nisi elit consequat ipsum
+                       <a href="./">http://t.co/CGIrdxIlI3</a>
+                     </span>
+                     <b>
+                       <a href="./">2 Days Ago</a>
+                     </b>
+                   </li>
+                   <li>
+                     <span>
+                       Sed ut perspiciatis unde omnis iste natus error sit
+                       voluptatem accusantium doloremque laudantium, totam rem
+                       aperiam, eaque ipsa quae ab illo inventore veritatis et
+                       quasi
+                       <a href="./">http://t.co/CGIrdxIlI3</a>
+                     </span>
+                     <b>
+                       <a href="./">3 Days Ago</a>
+                     </b>
+                   </li>
+                 </ul>
+               </div> */}
             </aside>
           </Slide>
         </div>
